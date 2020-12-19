1d78109504f2b9e85c93bfca553e3cef936f5375
Stanislav Kyfenko <stanislavk.kyfenko@gmail.com>
2020-12-19 15:33:03 +0100

    add comments

--- handler/priceService.js
+++ handler/priceService.js
@@ -4,35 +4,38 @@ const Price = require('./domain/Price');
 const {sendError, wrapResponse} = require('./utils');
 const {decorateBody, fetchProportions} = require('./fetcher')
 
+/**
+ * Retrieve all the possible proportions for current painting and make up the price for them proportionally
+ * and return results back
+ */
 const calculateItemProportions = async (body) => {
     if (body.sizeAndWeight.calculateProportion) {
         // fetch from dynamodb
         const proportions = await fetchProportions(body);
-        return proportions ? proportions.map(item => {
-            console.log(`item ${JSON.stringify(item)}`);
-            const result = {
-                size: `${item.sizeAndWeight.w}x${item.sizeAndWeight.h}x${body.sizeAndWeight.d} ${body.sizeAndWeight.unitOfMeasure}`,
-                price: new Price(item).calculatePriceWithDifferentSize(body.sizeAndWeight)
-            };
-            console.log(`result ${JSON.stringify(result)}`);
-
-            return result;
-        }) : null;
+        return proportions ?
+            proportions.map(item => {
+                return {
+                    size: `${item.sizeAndWeight.w}x${item.sizeAndWeight.h}x${body.sizeAndWeight.d} ${body.sizeAndWeight.unitOfMeasure}`,
+                    price: new Price(item).calculatePriceWithDifferentSize(body.sizeAndWeight)
+                };
+            })
+            :
+            null;
     }
 }
 
+/**
+ * Common method to calculate price
+ */
 const calculate = async (event) => {
     try {
         const body = await decorateBody(event);
         const price = new Price(body);
         const result = price.calculatePrice();
 
-        console.log(`calculated price ${result}`)
-
         const proportion = await calculateItemProportions(body)
         return wrapResponse(body, result, proportion);
     } catch (e) {
-        console.log(e);
         return sendError(e);
     }
 }
